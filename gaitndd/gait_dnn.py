420d3aa151d9732a5794de2fcccae5943d0b8b1f
sinwall <sinwall@snu.ac.kr>
2023-07-19 11:32:28 +0900

    determinism enabled

--- gaitndd/gait_dnn.py
+++ gaitndd/gait_dnn.py
@@ -1,3 +1,12 @@
+# # uncomment if in colab environment
+# from google.colab import drive
+# import os
+# drive.mount('/content/drive')
+# os.chdir('/content/drive/MyDrive/laboratory/geoml')
+# !git pull
+
+# !pip install wfdb
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -14,8 +23,10 @@ from tensorflow import keras
 import random
 from sklearn.metrics import accuracy_score
 
-def get_model(n_classes):
-    model = keras.Sequential([
+def get_model(n_classes, ts_shape=(300, 2), ts_only=True, xw_size=(128, )):
+    inputs_ts = keras.Input(ts_shape)
+    inputs_xw = keras.Input(xw_size)
+    layers_ts = keras.Sequential([
         keras.layers.LSTM(
             units=32,
             return_sequences=True
@@ -27,6 +38,7 @@ def get_model(n_classes):
         keras.layers.Conv1D(
             filters=16,
             kernel_size=16,
+            # padding='same',
             activation='relu'
         ),
         keras.layers.MaxPool1D(
@@ -36,6 +48,7 @@ def get_model(n_classes):
         keras.layers.Conv1D(
             filters=16,
             kernel_size=16,
+            # padding='same',
             activation='relu'
         ),
         keras.layers.MaxPool1D(
@@ -45,6 +58,7 @@ def get_model(n_classes):
         keras.layers.Conv1D(
             filters=16,
             kernel_size=16,
+            # padding='same',
             activation='relu'
         ),
         keras.layers.MaxPool1D(
@@ -52,11 +66,31 @@ def get_model(n_classes):
             strides=4
         ),
         keras.layers.Flatten(),
-        keras.layers.Dense(
+        keras.layers.BatchNormalization()
+    ])
+    final_layer = keras.layers.Dense(
             units=n_classes,
             activation='softmax'
         )
-    ])
+    outputs_ts = layers_ts(inputs_ts)
+    if ts_only:
+        outputs = outputs_ts
+    else:
+        layers_xw = keras.Sequential([
+            keras.layers.Dense(
+                units=8,
+                activation='relu'
+            ),
+            keras.layers.BatchNormalization(),
+        ])
+        outputs_xw = layers_xw(inputs_xw)
+        outputs = tf.concat([outputs_ts, outputs_xw], axis=-1)
+    outputs = final_layer(outputs)
+
+    model = keras.Model(
+        inputs=[inputs_ts, inputs_xw],
+        outputs=outputs
+    )
     model.compile(
         optimizer=keras.optimizers.Adam(1e-3),
         loss='sparse_categorical_crossentropy',
@@ -72,7 +106,7 @@ def main():
     signals_pre = np.empty((n_indivs, 90000, 2))
     labels = []
     ts_files = []
-    input_path = f'E:/database/gait-in-neurodegenerative-disease-database-1.0.0'
+    input_path = f'../input/gait-in-neurodegenerative-disease-database-1.0.0'
     for file_name in sorted(os.listdir(f'{input_path}')):
         if not file_name.endswith('hea'):
             continue
@@ -196,37 +230,72 @@ def main():
     #     )
     #     features_ts[seg_indivs==i] = f(np.arange(0, signals.shape[1], seg_len) + 0.5*seg_len)
 
-    keras.backend.clear_session()
-    tf.random.set_seed(42)
-    np.random.seed(42)
-    random.seed(42)
+    import sys
+    sys.path.append(os.path.abspath('./'))
+    from tools import Embedder, Weighting, SineFilter, calculate_weighting_vectors
 
-    # subcls = ['als', 'control']
-    subcls = ['control', 'hunt']
-    mask_subcls = np.isin(seg_labels, subcls)
+    embedder = Embedder(lag=24, reduce=0, dim_raw=2, channel_last=True)
+    x = embedder.transform(segs)
+    w_filename = './output/w_gaitndd_20230717.npy'
+    if not os.path.isdir('./output'):
+        os.mkdir('./output')
+    if os.path.isfile(w_filename):
+        w = np.load(w_filename)
+    else:
+        w = calculate_weighting_vectors(10*x)
+        np.save(w_filename, w)
 
-    X = segs
-    y_true = seg_labels.copy()
-    # y_true = seg_indivs.copy()
-    y_pred = y_true.copy()
+    file_log = open('./output/log_20230719.csv', 'w')
+    file_log.write('random_state,ts_only_true,ts_only_false\n')
 
-    for test_indiv in np.unique(seg_indivs[np.isin(seg_labels, subcls)]):
-        print(test_indiv, end=' ')
-        mask_train = (seg_indivs != test_indiv) & mask_subcls & mask_non_outlier
-        mask_test = (seg_indivs == test_indiv) & mask_subcls & mask_non_outlier
-        y = 0*(y_true == subcls[0])
-        for i in range(1, len(subcls)):
-            y += i*(y_true == subcls[i])
+    for random_state in range(42, 42+100):
+        sine_filter = SineFilter(dim=x.shape[-1], n_filters=128, scale=1e1, random_state=random_state)
+        sine_0d = sine_filter.apply(x, w[..., np.newaxis], batch_size=256)
 
+        keras.backend.clear_session()
+        tf.keras.utils.set_random_seed(random_state)
+        tf.config.experimental.enable_op_determinism()
 
-        model = get_model(len(subcls))
-        model.fit(
-            X[mask_train], y[mask_train],
-            epochs=10,
-            batch_size=64,
-            verbose=0
-        )
-        y_pred[mask_test] = np.vectorize(subcls.__getitem__)(np.argmax(model.predict(X[mask_test], verbose=0), axis=-1))
-        acc = 1e2*accuracy_score(y_true[mask_test], y_pred[mask_test])
-        print(acc)
-    1e2*accuracy_score(y_true, y_pred)
+        subcls = ['als', 'control']
+        # subcls = ['control', 'hunt']
+        mask_subcls = np.isin(seg_labels, subcls)
+
+        X = segs
+        y_true = seg_labels.copy()
+        # y_true = seg_indivs.copy()
+        y_pred = y_true.copy()
+        y_pred_another = y_pred.copy()
+
+        for test_indiv in np.unique(seg_indivs[np.isin(seg_labels, subcls)]):
+            print(test_indiv, end=' ')
+            mask_train = (seg_indivs != test_indiv) & mask_subcls & mask_non_outlier
+            mask_test = (seg_indivs == test_indiv) & mask_subcls & mask_non_outlier
+            y = 0*(y_true == subcls[0])
+            for i in range(1, len(subcls)):
+                y += i*(y_true == subcls[i])
+
+            for ts_only in (True, False):
+                model = get_model(len(subcls), ts_shape=X.shape[1:], xw_size=sine_0d.shape[-1], ts_only=ts_only)
+                model.fit(
+                    (X[mask_train], sine_0d[mask_train]), y[mask_train],
+                    epochs=10,
+                    batch_size=64,
+                    verbose=0
+                )
+                predicted = np.vectorize(subcls.__getitem__)(np.argmax(model.predict((X[mask_test], sine_0d[mask_test]), verbose=0), axis=-1))
+                if ts_only:
+                    y_pred[mask_test] = predicted
+                else:
+                    y_pred_another[mask_test] = predicted
+            acc = 1e2*accuracy_score(y_true[mask_test], y_pred[mask_test])
+            acc_another = 1e2*accuracy_score(y_true[mask_test], y_pred_another[mask_test])
+            print(acc, acc_another)
+        print( 1e2*accuracy_score(y_true, y_pred) )
+        print( 1e2*accuracy_score(y_true, y_pred_another) )
+        file_log.write(f'{random_state},{1e2*accuracy_score(y_true, y_pred)},{1e2*accuracy_score(y_true, y_pred_another)}\n')
+        file_log.flush(); os.fsync(file_log.fileno())
+        
+    file_log.close()
+
+if __name__ == '__main__':
+    main()
